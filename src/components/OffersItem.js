5441b242f71f6d64d496797d24756797b3aba30d
mother4 <mother4@mother4s-MacBook-Pro.local>
2024-11-19 12:57:19 +0100

    step 1 intro

--- src/components/OffersItem.js
+++ src/components/OffersItem.js
@@ -5,17 +5,11 @@ const OffersItem = ({ created_at, photos, price, title }) => {
   const created = new Date(created_at);
   return (
     <div className="offersitem">
-      <picture>
-        {photos && photos[0] && <img src={photos[0].url} alt="pic" />}
-      </picture>
+      <picture>Photo</picture>
       <nav>
-        <span>{title}</span>
-        <span>{price} €</span>
-        <span>
-          Annonce créée le {created.getDay()} {created.getMonth()}{" "}
-          {created.getFullYear()} à {created.getHours()} h et{" "}
-          {created.getMinutes()}mn
-        </span>
+        <span>titre</span>
+        <span>prix</span>
+        <span>Annonce créée le ...</span>
       </nav>
     </div>
   );
