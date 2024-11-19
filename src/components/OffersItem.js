11998821ab3853cdd4763293ee0e1735d8a4c666
mother4 <mother4@mother4s-MacBook-Pro.local>
2024-11-19 12:04:39 +0100

    Etape - 1 preparation

--- src/components/OffersItem.js
+++ src/components/OffersItem.js
@@ -1,6 +1,23 @@
 import React from "react";
+import "./OffersItem.css";
 
 const OffersItem = ({ created_at, photos, price, title }) => {
-  return <div className="offersitem"></div>;
+  const created = new Date(created_at);
+  return (
+    <div className="offersitem">
+      <picture>
+        {photos && photos[0] && <img src={photos[0].url} alt="pic" />}
+      </picture>
+      <nav>
+        <span>{title}</span>
+        <span>{price} €</span>
+        <span>
+          Annonce créée le {created.getDay()} {created.getMonth()}{" "}
+          {created.getFullYear()} à {created.getHours()} h et{" "}
+          {created.getMinutes()}mn
+        </span>
+      </nav>
+    </div>
+  );
 };
 export default OffersItem;
