ab63b5c104c3fdbf6f8d50042ff4d95de13c91fc
mother4 <mother4@mother4s-MacBook-Pro.local>
2024-11-21 15:57:21 +0100

    corr

--- src/containers/Publish.js
+++ src/containers/Publish.js
@@ -8,22 +8,61 @@ import "./Publish.css";
 import { apiUrl, store_id } from "../config";
 
 const Upload = ({ token, username }) => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const navigate = useNavigate();
+
+  const createProduct = async (e) => {
+    e.preventDefault();
+    try {
+      const data = new FormData();
+      data.append("title", title); //permet d'inserer title dans data
+      data.append("description", desc);
+      data.append("price", price);
+      data.append("store_id", store_id);
+      data.append("creator", "pierre");
+
+      await axios.post(apiUrl + "/product", data);
+      toast("l'offre est bien en ligne !", { type: "success" });
+      navigate("/");
+    } catch (e) {
+      console.log(e);
+      toast("il y a eu un probleme", { type: "error" });
+      navigate("/");
+    }
+  };
+
   return (
     <div className="publish formpage">
-      <form
-        onSubmit={async (event) => {
-          event.preventDefault();
-          //formadata va permettre de preparer le fichier pour pouvoir l'envoyer
-        }}
-      >
+      <form onSubmit={createProduct}>
         <h1>Déposer une annonce</h1>
         <hr></hr>
         <h2>Titre de l'annonce*</h2>
-        <input type="text"></input>
+        <input
+          value={title}
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+          type="text"
+        ></input>
+        <h2>Prix *</h2>
+        <input
+          type="number"
+          value={price}
+          onChange={(event) => {
+            setPrice(event.target.value);
+          }}
+        ></input>
         <h2>Texte de l'annonce*</h2>
-        <input type="text"></input>
-        <h2>Prix*</h2>
-        <input type="text"></input>
+        <input
+          type="text"
+          value={desc}
+          onChange={(event) => {
+            setDesc(event.target.value);
+          }}
+        ></input>
         <h2>Photo*</h2>
 
         <input type="submit" value="valider"></input>
